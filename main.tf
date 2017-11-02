5363957f917b4cafcce99e1f9be84b7dc4619ff7
george.kostaras <georgios.kostaras@hmcts.net>
2017-11-02 23:29:18 +0000

    Initial commit, creation of variables, providers and resource group

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "production" {
+    name  = "terraform-ansible-template"
+    location = "West Europe"
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable azure_subscription_id {
+    type = "string"
+}
+
+variable azure_client_id {
+    type = "string"
+}
+
+variable azure_client_secret {
+    type = "string"
+}
+
+variable azure_tenant_id {
+    type = "string"
+}
